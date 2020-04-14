1b3a3aa266cad656f9bd8a3989b01aae68d5d6ac
Koray Ozkal <koryozkal@gmail.com>
2020-04-14 20:11:59 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot (no_argument = "Squawk!")
-  puts "#{no_argument}"
-  "Squawk!"
+def parrot (bird = "Pretty bird!")
+# returns the phrase	  puts "#{bird}"
+  bird
+end
+
+def parrot (squawk = "Squawk!")
+  puts "#{squawk}"
+  squawk
 end 
