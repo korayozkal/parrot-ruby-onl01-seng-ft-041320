ba4dbaf558057171b77137b1000b7db04140d701
Koray Ozkal <koryozkal@gmail.com>
2020-04-14 19:41:05 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot
-  puts "Squawk!"
-  "Squawk!"
-end
+
 
 def parrot(phrase, phrase ="Pretty bird!")
 puts phrase
