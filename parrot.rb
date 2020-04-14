50840cb1ce90b6b56410df4f7adb318d4142d74e
Koray Ozkal <koryozkal@gmail.com>
2020-04-14 20:08:45 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot (no_argument = "Squawk!")
+def parrot (argument, no_argument = "Squawk!")
   puts "#{no_argument}"
   "Squawk!"
 end 
